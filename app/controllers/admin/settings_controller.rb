b3cd6effc76ecb12718ee5ca992e3d711c2d9c56
Jim Gay <jim@saturnflyer.com>
2008-06-25 16:01:24 -0400

    adding ability to add new setting

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,7 +1,20 @@
 class Admin::SettingsController < ApplicationController
   
+  only_allow_access_to :new, :create, :destroy
+    :when => :admin,
+    :denied_url => {:action => 'index'},
+    :denied_message => "See your administrator if you'd like to create a new setting."
+  
   def index
     @settings = Radiant::Config.find_all_as_tree
+  end
+  
+  def new
+  end
+  
+  def create
+    Radiant::Config.find_or_create_by_key(params[:setting]['key']).update_attributes(params[:setting])
+    redirect_to admin_settings_url
   end
   
   def edit
