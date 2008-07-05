b239257307d9b7095b9fd3254dae0bf01b9d66d0
Tim Gossett <tim@mrgossett.com>
2008-07-05 13:52:47 -0400

    changed "name" attribute to "key" and moved the tags to the config namespace

--- lib/settings_tags.rb
+++ lib/settings_tags.rb
@@ -3,16 +3,20 @@ module SettingsTags
 
   class TagError < StandardError; end
   
+  tag "config" do |tag|
+    tag.expand
+  end
+  
   desc %{
-    Renders the Radiant::Config setting specified by the required 'name' attribute.
+    Renders the Radiant::Config setting specified by the required 'key' attribute.
   
   *Usage*:
 
-  <pre><code><r:setting name="admin.title" /></code></pre>
+  <pre><code><r:config:setting key="admin.title" /></code></pre>
   }
-  tag "setting" do |tag|
-    raise TagError, "'name' attribute required" unless name = tag.attr['name']
-    Radiant::Config["#{name}"]
+  tag "config:setting" do |tag|
+    raise TagError, "'key' attribute required" unless name = tag.attr['key']
+    Radiant::Config["#{key}"]
   end
 
 end
