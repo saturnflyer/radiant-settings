40bc7e18859a5db1caec3a7d987c56babcd1d3b8
Jim Gay <jim@saturnflyer.com>
2008-10-22 23:03:32 -0400

    add spec for protected value

--- spec/views/admin/settings/index_view_spec.rb
+++ spec/views/admin/settings/index_view_spec.rb
@@ -16,6 +16,13 @@ describe "/admin/settings/index" do
     @title.stub!(:protected?).and_return(false)
     @title.stub!(:protected_value).and_return('Radiant CMS')
     
+    @password = mock_model(Radiant::Config)
+    @password.stub!(:key).and_return('account.password')
+    @password.stub!(:value).and_return('super secret')
+    @password.stub!(:description).and_return('password')
+    @password.stub!(:protected?).and_return(true)
+    @password.stub!(:protected_value).and_return('********')
+    
     assigns[:settings] = {
       'admin' => {
         'title' => @title
@@ -24,6 +31,9 @@ describe "/admin/settings/index" do
         'page' => {
           'parts' => @parts
         }
+      },
+      'account' => {
+        'password' => @password
       }
     }
     
@@ -39,5 +49,12 @@ describe "/admin/settings/index" do
     response.should have_tag('td', 'Page')
     response.should have_tag('td', 'Parts')
     response.should have_tag('td', 'body, extended')
+  end
+  
+  it "should display password settings as '********'" do
+    response.should have_tag('tr') do
+      with_tag('td','Password')
+      with_tag('td', '********')
+    end
   end
 end
