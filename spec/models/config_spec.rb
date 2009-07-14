d93d9d78436960f5d40ec8d23067a92d105176bf
Taylor Barstow <taylorbarstow@gmail.com>
2009-07-14 23:23:16 +0800

    avoid prematurely truncating paths with repeated key(s)
    Signed-off-by: Jim Gay <jim@saturnflyer.com>

--- spec/models/config_spec.rb
+++ spec/models/config_spec.rb
@@ -5,6 +5,7 @@ describe Radiant::Config, 'find_all_as_tree' do
     @title  = Radiant::Config.create(:key => 'admin.title', :value => 'Radiant CMS')
     @parts  = Radiant::Config.create(:key => 'defaults.page.parts', :value => 'body, extended')
     @status = Radiant::Config.create(:key => 'defaults.page.status', :value => 'draft')
+    @repetitive = Radiant::Config.create(:key => 'repetitive.setting.setting', :value => 'foo')
   end
   
   it "should find all settings and return a tree as a nested hash" do
@@ -12,5 +13,6 @@ describe Radiant::Config, 'find_all_as_tree' do
     result['admin']['title'].should             == @title
     result['defaults']['page']['parts'].should  == @parts
     result['defaults']['page']['status'].should == @status
+    result['repetitive']['setting']['setting'].should == @repetitive
   end
 end
