4b9c49c9628b9f9a2a7b4438a2d9a7ed55be9aca
Jim Gay <jim@saturnflyer.com>
2009-12-06 20:24:55 -0500

    no longer needed for new installations, but maintain the migration for existing sites

--- db/migrate/002_add_settings_roles.rb
+++ db/migrate/002_add_settings_roles.rb
@@ -3,7 +3,7 @@ class AddSettingsRoles < ActiveRecord::Migration
   class Config < ActiveRecord::Base; end
   
   def self.up
-    Config.create!(:key => 'roles.settings', :value => 'admin', :description => 'List of user roles that may see the settings tabs.')
+    # Config.create!(:key => 'roles.settings', :value => 'admin', :description => 'List of user roles that may see the settings tabs.')
   end
   
   def self.down
