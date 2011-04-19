a40e1d043e44b31c5932f53c12c9a09905c6c478
Andrew vonderLuft <avonderluft@avlux.net>
2011-04-19 14:02:43 -0700

    adjust specs for pre-populated configs

--- spec/models/config_spec.rb
+++ spec/models/config_spec.rb
@@ -2,9 +2,9 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Radiant::Config, 'find_all_as_tree' do
   before do
-    @title  = Radiant::Config.create(:key => 'admin.title', :value => 'Radiant CMS')
-    @parts  = Radiant::Config.create(:key => 'defaults.page.parts', :value => 'body, extended')
-    @status = Radiant::Config.create(:key => 'defaults.page.status', :value => 'draft')
+    @title  = Radiant::Config.find_or_create_by_key(:key => 'admin.title', :value => 'Radiant CMS')
+    @parts  = Radiant::Config.find_or_create_by_key(:key => 'defaults.page.parts', :value => 'body, extended')
+    @status = Radiant::Config.find_or_create_by_key(:key => 'defaults.page.status', :value => 'draft')
     @repetitive = Radiant::Config.create(:key => 'repetitive.setting.setting', :value => 'foo')
   end
   
